1ada74d650c79fe21c3662f616bf3fb6775d642e
Alban RENIER <renier.alban@gmail.com>
2022-09-06 17:21:41 -0300

    creation de la homepage

---
+++ README.md
@@ -0,0 +1,28 @@
+# AMAZ ZONE SHOP - Challenge La Capsule 
+
+Utiliser les sessions sur Express pour gérer votre panier.
+
+## 1 -  MISE EN PLACE DE L’ENVIRONNEMENT
+
+👉 Récupérez les ressources mises à disposition.
+
+👉 Installez les dépendances (npm install).
+
+## 2- HomePage
+
+👉 Utilisez la route “/” pour afficher la homePage ci-dessous.
+
+👉 Vous devez boucler sur votre tableau de produits afin d’afficher chaque card de produit.
+👉 Mettez en place une mécanique en utilisant les sessions pour ajouter les produits sélectionnés dans votre panier.
+
+Au clic sur le bouton, vous devez mettre à jour le compteur du panier situé en haut à droite.
+
+![](https://lh3.googleusercontent.com/b4AVQDkyOnLXEvtPgNHgyLftKcd474mgvdFXxl29MezRTiaOFckLGjUosL_VJfa-dI5WKI9GqHrx2NMQzDQikMcb4ymj1xe6Qx2KpiIq_RZCv11V8qw80GgfJ6fpKTfieg)
+
+## PANIER
+
+👉 Créez la route “/basket” qui affichera les informations détaillées du panier.
+
+👉 Il faudra utiliser les sessions pour gérer votre panier.
+
+👉 Pensez à créer le fichier basket.ejs.
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,76 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 var Products = [
   {
     name: "Apple watch",
     price: 300,
-    image: "apple-watch"
-  }, {
+    image: "apple-watch",
+  },
+  {
     name: "Porte document",
     price: 76,
-    image: "porte-doc"
-  }, {
+    image: "porte-doc",
+  },
+  {
     name: "DJI mavic air",
     price: 989,
-    image: "dji-mavic-air"
-  }, {
+    image: "dji-mavic-air",
+  },
+  {
     name: "Oculus",
     price: 342,
-    image: "oculus"
-  }, {
+    image: "oculus",
+  },
+  {
     name: "Bose QC35",
     price: 155,
-    image: "bose-qc35"
-  }, {
+    image: "bose-qc35",
+  },
+  {
     name: "Xiaomi-m365",
     price: 674,
-    image: "xiaomi-m365"
-  }, {
+    image: "xiaomi-m365",
+  },
+  {
     name: "BRIG Eagle 380",
     price: 15500,
-    image: "BRIG-Eagle-380"
-  }, {
+    image: "BRIG-Eagle-380",
+  },
+  {
     name: "Linda Razer",
     price: 897,
-    image: "linda"
-  }, {
+    image: "linda",
+  },
+  {
     name: "Fort 500",
     price: 67,
-    image: "fort-500"
-  }, {
+    image: "fort-500",
+  },
+  {
     name: "OnePlus 6",
     price: 540,
-    image: "one-plus6"
+    image: "one-plus6",
+  },
+];
+// Route pour Index HomePage
+router.get("/", function (req, res, next) {
+  if (req.session.basket == undefined) {
+    req.session.basket = [];
   }
-]
-
-router.get('/', function(req, res, next) {
-  res.render('index', {Products});
+  res.render("index", { Products, item: req.session.basket.length });
 });
 
-router.get('/basket', function(req, res, next) {
-  res.render('basket');
+router.post("/buy", function (req, res, next) {
+  req.session.basket.push(Products[req.body.position]);
+  res.render("index", { Products, item: req.session.basket.length });
+});
+
+router.get("/basket", function (req, res, next) {
+  res.render("basket", {
+    basket: req.session.basket,
+    item: req.session.basket.length,
+  });
 });
 
 module.exports = router;
